aa4474e3879e5ff631a4d07fe6d5119c5e54a25a
Sokheang-Eang <107766352+Sokheang-Eang@users.noreply.github.com>
2024-10-31 11:00:01 +0700

    Update inverseKinematics.m

--- Omni Drive/inverseKinematics.m
+++ Omni Drive/inverseKinematics.m
@@ -3,14 +3,14 @@ clc, close all, clear all;
 Lx = 0.3;   % Distance from center to wheel (meters)
 Ly = 0.3;   % Distance from center to wheel (meters)
 %% Robot Speed
-Vx = 1.0;     % Desired velocity in x (m/s)
+Vx = 1.0;      % Desired velocity in x (m/s)
 Vy = -1.0;     % Desired velocity in y (m/s)
-omega = 0.0;  % Desired angular velocity (rad/s)
+omega = 0.0;   % Desired angular velocity (rad/s)
 %% Inverse Kinematics for Mecanum Wheels
-fl = (Vx*cos(pi/4) - Vy*sin(pi/4) - (Lx + Ly)*omega);
-fr = (Vx*cos(3*pi/4) - Vy*sin(3*pi/4) + (Lx + Ly)*omega);
-rl = (Vx*cos(5*pi/4) - Vy*sin(5*pi/4) - (Lx + Ly)*omega);
-rr = (Vx*cos(7*pi/4) - Vy*sin(7*pi/4) + (Lx + Ly)*omega);
+fr = (Vx*sin(pi/4) - Vy*cos(pi/4) - (Lx + Ly)*omega);
+fl = (Vx*sin(3*pi/4) - Vy*cos(3*pi/4) - (Lx + Ly)*omega);
+rl = (Vx*sin(5*pi/4) - Vy*cos(5*pi/4) - (Lx + Ly)*omega);
+rr = (Vx*sin(7*pi/4) - Vy*cos(7*pi/4) - (Lx + Ly)*omega);
 %% Display the wheel velocities
 fprintf('\tFront Left: %.2f (rad/s)\n', fl);
 fprintf('\tFront Right: %.2f (rad/s)\n', fr);
